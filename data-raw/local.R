4fb1e80cf792f29fb11e289981161026a2509327
Victor Lee <kwangchun.lee.7@gmail.com>
2018-12-25 20:51:24 +0900

    지방선거 시도지사 데ì변수명 한글화

--- data-raw/local.R
+++ data-raw/local.R
@@ -1,87 +1,65 @@
-# packages -------------
+# 2018년 지방선거 개표결과 - 시도지사
+
+# 0. 팩키지 불러오기 -------------
 library(tidyverse)
 library(readxl)
 library(testthat)
 
-# read raw data -------------
-# Party name convention
-#
-# 민주당: "democracy"
-# 자유한국당: "freedom",
-# 바른미래당: "barun",
-# 정의당: "justice",
-# 민중당: "minjung",
-# 대한애국당: "korea",
-# 녹색당: "green",
-# 우리미래: "future",
-# 친박연대: "park"
+# 1. 데이터 불러오기 -----
+# 출처: https://bit.ly/2Q1fEcq
 
-var_names <- c("election_type","precinct","sido","sigungu","emd","type","electorate","vote",
-  "democracy","freedom",
-  "barun", "justice", "minjung",
-  "korea", "green", "future",
-  "park", "total", "invalid", "abstention")
+var_names <- c("선거종류", "선거구명", "시도명", "구시군명", "읍면동명", "구분",
+                 "선거인수", "투표수", "더불어민주당 박원순","자유한국당 김문수",
+                 "바른미래당 안철수","정의당 김종민","민중당 김진숙","대한애국당 인지연",
+                 "녹색당 신지예","우리미래 우인철","친박연대 최태현", "계", "무효투표수", "기권수")
 
-var_names_party <- ifelse(str_detect(var_names, "\r\r\n"),
-                          str_extract(var_names, ".+(?=\\r\\r\\n)"), # 당명만 추출
-                          var_names)
-
-local_2018_dat <- read_excel("data-raw/20180619-7지선-01-(시도지사)_읍면동별개표자료.xlsx", sheet="7지선-시도지사", skip=1,
+local_2018_dat <- read_excel("data-raw/20180619-7지선-01-(시도지사)_읍면동별개표자료.xlsx", sheet="7지선-시도지사", skip=0,
                              col_types = c(rep("text",6), rep("numeric", 14)))
 
-# local_2018 <- local_2018_dat %>%
-#     set_names(var_names_short) %>%
-#     select(-`선거종류`, -`계`) %>%
-#     filter(`시도명` != "합계") %>%
-#     filter(`읍면동명` != "계") %>%
-#     mutate(`구분` = case_when(is.na(`구분`) ~ `읍면동명`,
-#                               TRUE ~ `구분`)) %>%
-#     filter(`구분` != "소계")
+names(local_2018_dat) <- enc2native(names(local_2018_dat))
 
-local_2018_dat <- local_2018_dat %>%
+# 2. 데이터 정제 -----
+
+local_2018_df <- local_2018_dat %>%
     set_names(var_names) %>%
-    select(-election_type, -total) %>%
-    group_by(precinct) %>%
+    filter(row_number() !=1) %>%
+    dplyr::select(-`선거종류`, -`계`) %>%
+    group_by(`선거구명`) %>%
     nest()
 
-local_2018 <- local_2018_dat %>%
-    mutate(data_clean = map(data, ~ filter(., !is.na(sigungu)))) %>%
-    mutate(data_clean = map(data_clean, ~ filter(., emd !="계"))) %>%
-    mutate(data_clean = map(data_clean, ~ mutate(., type = ifelse(is.na(`type`), emd, type)))) %>% # because of NA
-    mutate(data_clean = map(data_clean, ~ filter(.,  type !="소계" ))) # %>%
-    # mutate(data_clean = map(data_clean, ~ filter(.,  !str_detect(type, "잘못") )))
-    # filter(sido_adm == "세종특별자치시") %>%
-    # pull(data2)
+local_2018 <- local_2018_df %>%
+    mutate(data_clean = map(data, ~ filter(., !is.na(`시도명`)))) %>%
+    mutate(data_clean = map(data_clean, ~ filter(., `읍면동명` !="계"))) %>%
+    mutate(data_clean = map(data_clean, ~ mutate(., `구분` = ifelse(is.na(`구분`), `읍면동명`, `구분`)))) %>% # because of NA
+    mutate(data_clean = map(data_clean, ~ filter(.,  `구분` !="소계" )))
 
-
-# data wrangling -------------
-
-# unit test -------------
+# 3. 단위테스트 검증 -------------
 
 test_that("지방선거 2018 후보득표검증", {
 
+    # 서울시장 후보 단위테스트 검정
     local_2018_seoul_check <- local_2018 %>%
-        filter(precinct == "서울특별시") %>%
+        filter(`선거구명` == "서울특별시") %>%
         pull(data_clean) %>% .[[1]] %>%
-        summarise(`democracy` = sum(`democracy`),
-                  `freedom` = sum(`freedom`),
-                  `barun` = sum(`barun`),
-                  `justice` = sum(`justice`),
-                  `minjung` = sum(`minjung`),
-                  `korea` = sum(`korea`),
-                  `green` = sum(`green`),
-                  `future` = sum(`future`),
-                  `park` = sum(`park`))
+        summarise(`더불어민주당 박원순` = sum(`더불어민주당 박원순`),
+                  `자유한국당 김문수`   = sum(`자유한국당 김문수`),
+                  `바른미래당 안철수`   = sum(`바른미래당 안철수`),
+                  `정의당 김종민`       = sum(`정의당 김종민`),
+                  `민중당 김진숙`       = sum(`민중당 김진숙`),
+                  `대한애국당 인지연`   = sum(`대한애국당 인지연`),
+                  `녹색당 신지예`       = sum(`녹색당 신지예`),
+                  `우리미래 우인철`     = sum(`우리미래 우인철`),
+                  `친박연대 최태현`     = sum(`친박연대 최태현`))
 
-    expect_that( local_2018_seoul_check$democracy, equals(2619497))
-    expect_that( local_2018_seoul_check$freedom,   equals(1158487))
-    expect_that( local_2018_seoul_check$barun,     equals(970374))
-    expect_that( local_2018_seoul_check$justice,   equals(81664))
-    expect_that( local_2018_seoul_check$minjung,   equals(22134))
-    expect_that( local_2018_seoul_check$korea,     equals(11222))
-    expect_that( local_2018_seoul_check$green,     equals(82874))
-    expect_that( local_2018_seoul_check$future,    equals(11599))
-    expect_that( local_2018_seoul_check$park,      equals(4021))
+    expect_that( local_2018_seoul_check$`더불어민주당 박원순`, equals(2619497))
+    expect_that( local_2018_seoul_check$`자유한국당 김문수`,   equals(1158487))
+    expect_that( local_2018_seoul_check$`바른미래당 안철수`,   equals(970374))
+    expect_that( local_2018_seoul_check$`정의당 김종민`,       equals(81664))
+    expect_that( local_2018_seoul_check$`민중당 김진숙`,       equals(22134))
+    expect_that( local_2018_seoul_check$`대한애국당 인지연`,   equals(11222))
+    expect_that( local_2018_seoul_check$`녹색당 신지예`,       equals(82874))
+    expect_that( local_2018_seoul_check$`우리미래 우인철`,     equals(11599))
+    expect_that( local_2018_seoul_check$`친박연대 최태현`,     equals(4021))
 })
 
 usethis::use_data(local_2018, overwrite = TRUE)
