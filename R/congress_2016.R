da5797915b96fce87285496fdae58af94669ec60
Kwnaghcun Lee <kwangchun.lee.7@gmail.com>
2021-06-19 21:42:36 +0900

    update document

--- R/congress_2016.R
+++ R/congress_2016.R
@@ -1,7 +1,8 @@
 #' 제20대 2016년 국회의원 선거
 #'
+#' @name congress_2016
 #' @source http://www.nec.go.kr/portal/bbs/view/B0000338/37352.do?menuNo=200185&searchYear=&searchMonth=&searchWrd=%EA%B0%9C%ED%91%9C%EA%B2%B0%EA%B3%BC&searchCnd=1&viewType=&pageIndex=1&section=&searchOption1=
 #' @format list-column을 갖는 티블
 #' @examples
 #'   속초시 <- congress_2016 %>% filter(precinct == "속초시") %>% pull(data_clean) %>% .[[1]]
-'congress_2016'
+NULL
--- R/precinct_2016.R
+++ R/precinct_2016.R
@@ -1,7 +1,8 @@
 #' 국회의원 2016년 선거구
 #'
+#' @name precinct_2016
 #' @source http://www.nec.go.kr/portal/bbs/view/B0000338/32767.do?menuNo=200185
 #' @format tibble with columns
 #' @examples
 #'   precinct_tbl <- precinct_2016
-'precinct_2016'
+NULL
